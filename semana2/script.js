8e4a0ff2974ece743f53871872f438ce6a1b1b9e
yandiraMN <yandiramorales3118@gmail.com>
2024-01-08 10:33:40 -0400

    semana1

---
+++ semana2/script.js
@@ -0,0 +1,9 @@
+function agregarTarea() {
+
+    var nuevaTarea = document.getElementById("nueva-tarea").value;
+    var nuevaLista = document.createElement("li");
+    nuevaLista.textContent = nuevaTarea;
+    document.getElementById("tareas-lista").appendChild(nuevaLista);
+    document.getElementById("nueva-tarea").value="";
+
+}
