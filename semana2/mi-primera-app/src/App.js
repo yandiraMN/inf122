e2670dfdafb6ff5132938c797fab4feabc36660b
yandiraMN <yandiramorales3118@gmail.com>
2024-01-12 11:07:37 -0400

    sem2

--- semana2/mi-primera-app/src/App.js
+++ semana2/mi-primera-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Evento from './components/Evento.js'
 import EventoB from './components/EventoB.js'
-
 function App() {
   return (
     <div className="App">
@@ -25,7 +24,6 @@ function App() {
         description="
         ¡Hola Pythonista!
         Acompáñanos al MeetUp mensual de la comunidad Python La Paz
-
         En este MeetUp tendremos la participación de Valery Calderon Briz, Ingeniera en Telecomunicaciones actualmente trabajando como Ingeniera de datos Senior en One.Five
         Anteriormente Co-organizadora de Pyladies MX y Líder de la comunidad Python Guatemala
         ha impartido conferencias y talleres en diversas PyCon en múltiples países.
