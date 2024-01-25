f0eae6edf85764f08e7257256c510a9d4acaf810
yandiraMN <yandiramorales3118@gmail.com>
2024-01-25 10:41:53 -0400

    perrito

--- semana4/galeria-perritos/src/app/components/foto.js
+++ semana4/galeria-perritos/src/app/components/foto.js
@@ -3,17 +3,18 @@ import style from './foto.module.css'
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 export default function Foto() {
-    const [perrito, setPerrito]=useState ("/vercel.svg");   {/*estado*/}
+    const [perrito, setPerrito]=useState ("/vercel.svg");   {/*estado en react*/}
+    const [estado, setEstado]=useState ("esperando");
     const url = "https://dog.ceo/api/breeds/image/random";
-    useEffect(() => {
+    useEffect(() => {          {/*el useefect espera que algo se modifique*/ }
         fetch(url)
             .then(res => res.json())
-            .then(data => {
-                setPerrito(data.message)
-            })
+            .then(data => {setPerrito(data.message), setEstado(data.status)
+            });
     }, [])
     return (
         <div className={style.galeria}>
+            <h1>{estado}</h1>
             <Image src={perrito} alt="perrito" width={250} height={250}  />
         </div>
     );
